6b4f699908da396e9243c0fba550abf7d6817a2d
shadmanhossain <shadmanh@sfu.ca>
2023-10-05 13:34:13 -0700

    Created fragments used in the MainActivity.kt

--- app/src/main/java/com/example/shadman_hossain_myruns1/FragmentAdapter.kt
+++ app/src/main/java/com/example/shadman_hossain_myruns1/FragmentAdapter.kt
@@ -1,4 +1,16 @@
 package com.example.shadman_hossain_myruns1
 
-class FragmentAdapter {
+import android.app.Activity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentAdapter(activity: FragmentActivity, var list: ArrayList<Fragment>):FragmentStateAdapter(activity) {
+    override fun createFragment(position: Int): Fragment {
+        return list[position]
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
 }
--- app/src/main/java/com/example/shadman_hossain_myruns1/MainActivity.kt
+++ app/src/main/java/com/example/shadman_hossain_myruns1/MainActivity.kt
@@ -15,16 +15,46 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var startFragement:Fragment
+    private lateinit var historyFragment: Fragment
+    private lateinit var settingsFragment: Fragment
+    private lateinit var fragments:ArrayList<Fragment>
+    private lateinit var fragmentAdapter: FragmentAdapter
+    private lateinit var tabLayoutMediator: TabLayoutMediator
+    private val fragmentTitles = arrayOf("Start", "History", "Settings")
+    private lateinit var tabConfigurationStrategy:TabConfigurationStrategy
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        val intent = Intent(this, Profile::class.java)
-        startActivity(intent)
+//        val intent = Intent(this, Profile::class.java)
+//        startActivity(intent)
+        viewPager2 = findViewById(R.id.viewPager2Container)
+        tabLayout = findViewById(R.id.tabLayout)
+        startFragement = StartFragment()
+        historyFragment = HistoryFragment()
+        settingsFragment = SettingsFragment()
+        fragments = ArrayList()
+        fragments.add(startFragement)
+        fragments.add(historyFragment)
+        fragments.add(settingsFragment)
+        fragmentAdapter = FragmentAdapter(this,fragments)
+        viewPager2.adapter = fragmentAdapter
+        tabConfigurationStrategy = TabConfigurationStrategy{tab, position -> tab.text = fragmentTitles[position]  }
+        tabLayoutMediator = TabLayoutMediator(tabLayout,viewPager2,tabConfigurationStrategy)
+        tabLayoutMediator.attach()
     }
+
 }
