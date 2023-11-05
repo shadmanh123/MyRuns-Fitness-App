57575bc3343d8519f7cfb7ad9e6b632f3389953e
shadmanhossain <shadmanh@sfu.ca>
2023-11-04 19:16:31 -0700

    Created ExerciseDatabaseDao for the exercise database

--- app/src/main/java/com/example/shadman_hossain_myruns1/ExerciseDatabaseDao.kt
+++ app/src/main/java/com/example/shadman_hossain_myruns1/ExerciseDatabaseDao.kt
@@ -1,4 +1,19 @@
 package com.example.shadman_hossain_myruns1
 
-class ExerciseDatabaseDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ExerciseDatabaseDao {
+
+    @Insert
+    suspend fun insertExercise(exerciseEntry: ExerciseEntry)
+
+    @Query("SELECT * FROM exercise_table")
+    fun getAllEntries(): Flow<List<ExerciseEntry>>
+
+    @Query("DELETE FROM exercise_table WHERE id = :key")
+    suspend fun deleteExercise(key: Long)
 }
