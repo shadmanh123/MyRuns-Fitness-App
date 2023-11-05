223361591ced4687a6a333d5306c83516620cc0c
shadmanhossain <shadmanh@sfu.ca>
2023-11-04 19:47:31 -0700

    Created ExerciseDatabase for the exercise database

--- app/src/main/java/com/example/shadman_hossain_myruns1/ExerciseDatabase.kt
+++ app/src/main/java/com/example/shadman_hossain_myruns1/ExerciseDatabase.kt
@@ -1,4 +1,28 @@
 package com.example.shadman_hossain_myruns1
 
-class ExerciseDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ExerciseEntry::class], version = 1)
+abstract class ExerciseDatabase: RoomDatabase() {
+    abstract val exerciseDatabase: ExerciseDatabase
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ExerciseDatabase? = null
+
+        fun getInstance(context: Context): ExerciseDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                        ExerciseDatabase::class.java,"exercise_table").build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
