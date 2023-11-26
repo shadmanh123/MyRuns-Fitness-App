f24792ec33eb8f474879265a15e20f90ab83a423
shadmanhossain <shadmanh@sfu.ca>
2023-11-26 12:44:31 -0800

    Created WekaClassifier class

---
+++ app/src/main/java/com/example/shadman_hossain_myruns1/WekaClassifier.java
@@ -0,0 +1,57 @@
+package com.example.shadman_hossain_myruns1;
+
+class WekaClassifier {
+
+    public static double classify(Object[] i)
+            throws Exception {
+
+        double p = Double.NaN;
+        p = WekaClassifier.Na1f40b00(i);
+        return p;
+    }
+    static double Na1f40b00(Object []i) {
+        double p = Double.NaN;
+        if (i[0] == null) {
+            p = 0;
+        } else if (((Double) i[0]).doubleValue() <= 13.390311) {
+            p = 0;
+        } else if (((Double) i[0]).doubleValue() > 13.390311) {
+            p = WekaClassifier.N55c804181(i);
+        }
+        return p;
+    }
+    static double N55c804181(Object []i) {
+        double p = Double.NaN;
+        if (i[64] == null) {
+            p = 1;
+        } else if (((Double) i[64]).doubleValue() <= 14.534508) {
+            p = WekaClassifier.N12adf7a42(i);
+        } else if (((Double) i[64]).doubleValue() > 14.534508) {
+            p = 2;
+        }
+        return p;
+    }
+    static double N12adf7a42(Object []i) {
+        double p = Double.NaN;
+        if (i[4] == null) {
+            p = 1;
+        } else if (((Double) i[4]).doubleValue() <= 14.034383) {
+            p = WekaClassifier.N5a026f2c3(i);
+        } else if (((Double) i[4]).doubleValue() > 14.034383) {
+            p = 1;
+        }
+        return p;
+    }
+    static double N5a026f2c3(Object []i) {
+        double p = Double.NaN;
+        if (i[7] == null) {
+            p = 1;
+        } else if (((Double) i[7]).doubleValue() <= 4.804712) {
+            p = 1;
+        } else if (((Double) i[7]).doubleValue() > 4.804712) {
+            p = 2;
+        }
+        return p;
+    }
+}
+
